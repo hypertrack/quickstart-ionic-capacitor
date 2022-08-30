c922f5b0565ede4a05c25e2626e6c1f9871b5c11
ashish.pandey <ashish.pandey@synoriq.in>
2022-08-30 23:56:37 +0530

    quickstart-app

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# quickstart-ionic-capacitor
+# Ionic Quickstart for Hypertrack Capacitor Plugin
+
+![GitHub](https://img.shields.io/github/license/hypertrack/quickstart-ios.svg)
+![Android SDK](https://img.shields.io/badge/Android%20SDK-6.2.0-brightgreen.svg)
+
+[HyperTrack](https://www.hypertrack.com) lets you add live location tracking to your mobile app.
+Live location is made available along with ongoing activity, tracking controls and tracking outage with reasons.
+This repo contains an example ionic app that has everything you need to get started in minutes.
+
+## Create HyperTrack Account
+
+[Sign up](https://dashboard.hypertrack.com/signup) for HyperTrack and
+get your publishable key from the [Setup page](https://dashboard.hypertrack.com/setup).
+
+## Screenshots
+<table>
+<tr>
+<th colspan="2">
+Android
+</th>
+</tr>
+
+<tr>
+<td>
+<p align="center">
+<img src="Images/Device_1.jpeg" width="30%" height="30%">
+</p>
+</td>
+<td>
+<p align="center">
+<img src="Images/Device_2.jpeg" width="30%" height="30%">
+</p>
+</td>
+</tr>
+</table>
+
+
+### Set up Firebase
+
+1. [Set up Firebase Project for quickstart-ionic-capacitor](https://console.firebase.google.com/u/0/)
+2. Sign up for HyperTrack and [fill the FCM Key section in Android paragraph](https://dashboard.hypertrack.com/setup)
+
+## Build quickstart-ionic-capacitor
+
+Create a ionic build by [build command](https://ionicframework.com/docs/cli/commands/capacitor-build)
+
+In order to use this plugin, please update the Gradle, AndroidManifest.xml:
+
+In ```quickstart-ionic-capacitor/android/build.gradle``` update
+
+```
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            name 'hypertrack'
+            url  'https://s3-us-west-2.amazonaws.com/m2.hypertrack.com/'
+        }
+    }
+}
+```
+
+In ```quickstart-ionic-capacitor/android/variables.gradle``` update
+
+```
+minSdkVersion = 24
+compileSdkVersion = 31
+targetSdkVersion = 31
+```
+
+In ```quickstart-ionic-capacitor/android/app/src/main/AndroidManifest.xml``` update 
+
+```
+ <activity
+      android:configChanges="orientation|keyboardHidden|keyboard|screenSize|locale|smallestScreenSize|screenLayout|uiMode"
+      android:name="io.ionic.starter.MainActivity"
+      android:label="@string/title_activity_main"
+      android:theme="@style/AppTheme.NoActionBarLaunch"
+      android:launchMode="singleTask"
+      android:exported="true">
+
+      <intent-filter>
+          <action android:name="android.intent.action.MAIN" />
+          <category android:name="android.intent.category.LAUNCHER" />
+      </intent-filter>
+</activity>
+```
+## Dashboard
+
+Once your app is running, go to the [dashboard](https://dashboard.hypertrack.com/devices) where you can see a list of all your devices and their live location with ongoing activity on the map.
+
+## Support
+Join our [Slack community](https://join.slack.com/t/hypertracksupport/shared_invite/enQtNDA0MDYxMzY1MDMxLTdmNDQ1ZDA1MTQxOTU2NTgwZTNiMzUyZDk0OThlMmJkNmE0ZGI2NGY2ZGRhYjY0Yzc0NTJlZWY2ZmE5ZTA2NjI) for instant responses. You can also email us at help@hypertrack.com.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,275 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy } from '@angular/core';
+import { Blocker, HyperTrack, HyperTrackSdkInstance } from 'hypertrack-capacitor-plugin';
+import { AlertController } from '@ionic/angular';
+import { ChangeDetectorRef } from '@angular/core'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+  publishableKey: string = 'jNvgqDgY_j3yj-OwbTQtVcCoVGMOLkmllgV7KUtElL5j3g3nGwNEpLzdOO88KZ1E6rkeGmH7b9spUSFIYM5SkQ'; //'YOUR-PUBLISHABLE-KEY-HERE';
+  blockerList: Blocker[] = [];
+  hypertrackInstance: HyperTrackSdkInstance;
+  deviceId: Record<string, string> = {};
+  status: string = 'NA';
+  stateChangeListner;
+  error: string = 'NA';
+  location = 'NA';
+  isTracking;
+  availabilityStateChange;
+  constructor(private alertController: AlertController, private changeRef: ChangeDetectorRef) {
+    HyperTrack.enableDebugLogging();
+    this.initialize();
+  }
 
-  constructor() {}
+  async initialize() {
+    try {
+      await this.getBlocker();
+      const result = await HyperTrack.ininitialize(this.publishableKey);
+      this.hypertrackInstance = result.hyperTrackInstance;
+      await this.getDeviceId();
+      await this.addTrackingListener();
+      await this.hypertrackInstance.setDeviceName({ name: 'Quickstart Ionic' });
+      this.isTracking = await this.isTrackingStatus()
+      if (this.isTracking === true) {
+        this.status = 'Tracking has started...';
+      } else if (this.isTracking === false) {
+        this.status = `Tracking hasn't started...`;
+      }
+      this.changeRef.detectChanges();
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
 
+  async getBlocker() {
+    try {
+      this.blockerList = await HyperTrack.getBlockers();
+      if (this.blockerList.length > 0) {
+        this.blockerList.forEach(element => {
+          element.resolve();
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async start() {
+    try {
+
+      await this.getBlocker();
+      await this.hypertrackInstance.start();
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async stop() {
+    try {
+      const isTracking = await this.isTrackingStatus();
+      if (isTracking) {
+        await this.hypertrackInstance.stop();
+      }
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async getDeviceId() {
+    try {
+      this.deviceId = await this.hypertrackInstance.getDeviceId();
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async addTrackingListener() {
+    try {
+      await this.hypertrackInstance.addTrackingListener();
+      await this.getTrackingStateChange();
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async getTrackingStateChange() {
+    this.stateChangeListner = await this.hypertrackInstance.addListener(
+      'trackingStateChange',
+      (info: any) => {
+        if (info.status === 'start') {
+          this.status = 'Tracking has started...';
+        } else if (info.status === 'stop') {
+          this.status = 'Tracking has stopped...';
+        } else if (info.error) {
+          console.log(info.error);
+          this.status = JSON.stringify(info.error);
+        }
+        this.changeRef.detectChanges();
+      }
+    );
+  }
+
+  getLatestLocation() {
+    this.isTrackingStatus().then((status) => {
+      if (status) {
+        this.hypertrackInstance.getLatestLocation((res, err) => {
+          if (res) {
+            this.location = JSON.stringify(res.location);
+            this.changeRef.detectChanges();
+          } else {
+            console.log(err);
+            this.showAlert('Error', err);
+          }
+        });
+      }
+    });
+  }
+
+  async isTrackingStatus() {
+    try {
+      const res = await this.hypertrackInstance.isTracking();
+      return res.status;
+    } catch (error) {
+      this.showAlert('Error', error);
+    }
+  }
+
+  async mockLocations() {
+    try {
+      await this.hypertrackInstance.allowMockLocations();
+      this.showAlert('Message', 'Mock Locations allowed');
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async addGeoTag() {
+    try {
+      const geoTagObj = {
+        metadata: { "orderId": "ABC00001" },
+        coordinates: { latitude: 26.922070, longitude: 75.778885 }
+      };
+      await this.hypertrackInstance.addGeotag(geoTagObj);
+      this.showAlert('Message', JSON.stringify(geoTagObj));
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async setAvailability() {
+    try {
+      await this.hypertrackInstance.setAvailability({ isAvailable: true });
+      this.showAlert('Message', JSON.stringify({ isAvailable: true }));
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async getAvailability() {
+    try {
+      const res = await this.hypertrackInstance.getAvailability();
+      this.showAlert('Availability', JSON.stringify(res));
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async setTrackingNotificationProperties() {
+    try {
+      const propertiesObj = { title: "Tracking On", message: "Ionic SDK is tracking" };
+      await this.hypertrackInstance.setTrackingNotificationProperties(propertiesObj);
+      this.showAlert('Notification properties changed', JSON.stringify(propertiesObj));
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async requestPermissions() {
+    try {
+      await this.hypertrackInstance.requestPermissionsIfNecessary();
+      this.showAlert('Message', 'requestPermissionsIfNecessary called');
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async removeAvailabilityListener() {
+    try {
+      await this.hypertrackInstance.removeAvailabilityListener();
+      this.availabilityStateChange.remove();
+      this.showAlert('Message', 'addAvailability Listener removed');
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async addAvailabilityListener() {
+    try {
+      await this.hypertrackInstance.addAvailabilityListener();
+      this.getAvailabilityStateChange();
+      this.showAlert('Message', 'addAvailability Listener added');
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async syncDevice() {
+    try {
+      await this.hypertrackInstance.syncDeviceSettings();
+      this.showAlert('Message', 'syncDevice called');
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async setMetadata() {
+    try {
+      await this.hypertrackInstance.setDeviceMetadata({ appName: "quickstart-ionic-capacitor", appVersion: "1.0.0" });
+      this.showAlert('Data set successfully', JSON.stringify({ appName: "quickstart-ionic-capacitor", appVersion: "1.0.0" }));
+    } catch (error) {
+      console.log(error);
+      this.showAlert('Error', error);
+    }
+  }
+
+  async getAvailabilityStateChange() {
+    this.availabilityStateChange = await this.hypertrackInstance.addListener(
+      'availabilityStateChange',
+      (info: any) => {
+        this.showAlert('Availability State', JSON.stringify(info));
+      }
+    );
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+  
+  ngOnDestroy() {
+    this.hypertrackInstance.removeTrackingListener().then(() => {
+      this.stateChangeListner.remove();
+    });
+  }
 }
